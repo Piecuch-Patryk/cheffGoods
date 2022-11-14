c9adfaf1d66f5c2fb83428b84d2780e919321499
Patryk <patrykowo@gmail.com>
2022-11-14 18:21:06 +0100

    Create seeders.

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        for ($i=0; $i < 10; $i++) { 
+            Product::create([
+                'category_id' => 1,
+                'title' => 'Main Cours title #' . $i,
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit provident qui dolor earum nobis ipsam, cupiditate tempore odio.',
+                'price' => 1 . $i . 97,
+            ]);
+        }
+        for ($i=0; $i < 5; $i++) { 
+            Product::create([
+                'category_id' => 2,
+                'title' => 'Soup title #' . $i,
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit provident qui dolor earum nobis ipsam, cupiditate tempore odio.',
+                'price' => 1 . $i . 97,
+            ]);
+        }
+        for ($i=0; $i < 7; $i++) { 
+            Product::create([
+                'category_id' => 3,
+                'title' => 'Desert title #' . $i,
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit provident qui dolor earum nobis ipsam, cupiditate tempore odio.',
+                'price' => 1 . $i . 97,
+            ]);
+        }
+        for ($i=0; $i < 8; $i++) { 
+            Product::create([
+                'category_id' => 4,
+                'title' => 'Drink title #' . $i,
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit provident qui dolor earum nobis ipsam, cupiditate tempore odio.',
+                'price' => 1 . $i . 97,
+            ]);
+        }
+    }
+}
