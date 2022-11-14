829fb1b739b1053e0f7c0f4df54f0a3427418af6
Patryk <patrykowo@gmail.com>
2022-11-14 17:34:47 +0100

    Create database migrations.

---
+++ database/migrations/2022_11_10_133747_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('price');
+            $table->foreign('category_id')->on('categories')->references('id')
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
