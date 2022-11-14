46254db49bfbfbbb8274641d7d780b7f182f240d
Patryk <patrykowo@gmail.com>
2022-11-14 19:13:56 +0100

    Create products and categories controllers. Add routing to display categories and products.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Categories
+Route::get('/categories', [CategoryController::class,'index'])->name('categories');
 
-
-Route::get('/', function () {
-    return 'API';
-});
+// Products
+Route::get('/products', [ProductController::class, 'index'])->name('products');
