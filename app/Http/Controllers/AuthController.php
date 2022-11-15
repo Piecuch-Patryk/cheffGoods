db830f1a3feacff35e9de8a789e7726759b5c471
Patryk <patrykowo@gmail.com>
2022-11-15 13:05:19 +0100

    Add register, login and logout methods and routes. Add custom request to validate form's data.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+
+class AuthController extends Controller
+{
+    /**
+     * Create new user
+     *
+     * @param  \App\Http\Requests\StoreUserRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(StoreUserRequest $request)
+    {
+        $fields = $request->all();
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('cheffgoodstoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    /**
+     * Login user
+     * 
+     * @param \App\Http\Requests\LoginUserRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(LoginUserRequest $request)
+    {
+        $fields = $request->all();
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Wrong credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('cheffgoodstoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 200);
+    }
+
+    /**
+     * Delete user's auth token
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response([
+            'message' => 'Logged out',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,17 +17,30 @@ use App\Http\Controllers\ProductController;
 |
 */
 
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Auth
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+    // Category
+    Route::post('/category', [CategoryController::class, 'store']);
+    Route::put('/category/{category}', [CategoryController::class, 'update']);
+    Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
+    // Product
+    Route::post('/product', [ProductController::class, 'store']);
+    Route::put('/product/{product}', [ProductController::class, 'update']);
+    Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+});
+
+// Auth
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 // Categories
 Route::get('/categories', [CategoryController::class,'index']);
-Route::post('/category', [CategoryController::class, 'store']);
 Route::get('/category/{category}', [CategoryController::class, 'show']);
-Route::put('/category/{category}', [CategoryController::class, 'update']);
-Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
 
 // Products
 Route::get('/products', [ProductController::class, 'index']);
-Route::post('/product', [ProductController::class, 'store']);
 Route::get('/product/{product}', [ProductController::class, 'show']);
-Route::put('/product/{product}', [ProductController::class, 'update']);
-Route::delete('/product/{id}', [ProductController::class, 'destroy']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
